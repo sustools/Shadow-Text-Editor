fb81fd3ee1861912e0475f2eb4917b6cc7bc7c70
sustools <113345304+sustools@users.noreply.github.com>
2022-09-12 11:51:18 +0200

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Shadow_Text_Editor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txbEditor = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.chkDebug = new System.Windows.Forms.CheckBox();
+            this.chkProtected = new System.Windows.Forms.CheckBox();
+            this.lblVer = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(802, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.fileToolStripMenuItem_DropDownItemClicked);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // txbEditor
+            // 
+            this.txbEditor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEditor.Location = new System.Drawing.Point(12, 74);
+            this.txbEditor.Multiline = true;
+            this.txbEditor.Name = "txbEditor";
+            this.txbEditor.Size = new System.Drawing.Size(778, 344);
+            this.txbEditor.TabIndex = 1;
+            this.txbEditor.Text = " ";
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "All files (*.*)|*.*";
+            this.ofd.FilterIndex = 2;
+            this.ofd.Title = "Open File";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFileName.Location = new System.Drawing.Point(10, 47);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(146, 24);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "No File Opened";
+            // 
+            // sfd
+            // 
+            this.sfd.Filter = "All files (*.*)|*.*";
+            this.sfd.FilterIndex = 2;
+            this.sfd.Title = "Save File";
+            // 
+            // chkDebug
+            // 
+            this.chkDebug.AutoSize = true;
+            this.chkDebug.Location = new System.Drawing.Point(94, 28);
+            this.chkDebug.Name = "chkDebug";
+            this.chkDebug.Size = new System.Drawing.Size(88, 17);
+            this.chkDebug.TabIndex = 3;
+            this.chkDebug.Text = "Debug Mode";
+            this.chkDebug.UseVisualStyleBackColor = true;
+            // 
+            // chkProtected
+            // 
+            this.chkProtected.AutoSize = true;
+            this.chkProtected.Location = new System.Drawing.Point(16, 27);
+            this.chkProtected.Name = "chkProtected";
+            this.chkProtected.Size = new System.Drawing.Size(72, 17);
+            this.chkProtected.TabIndex = 4;
+            this.chkProtected.Text = "Protected";
+            this.chkProtected.UseVisualStyleBackColor = true;
+            this.chkProtected.CheckedChanged += new System.EventHandler(this.chkProtected_CheckedChanged);
+            // 
+            // lblVer
+            // 
+            this.lblVer.AutoSize = true;
+            this.lblVer.Location = new System.Drawing.Point(755, 28);
+            this.lblVer.Name = "lblVer";
+            this.lblVer.Size = new System.Drawing.Size(22, 13);
+            this.lblVer.TabIndex = 5;
+            this.lblVer.Text = "1.0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 430);
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.chkProtected);
+            this.Controls.Add(this.chkDebug);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.txbEditor);
+            this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "Shadow Text Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox txbEditor;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.CheckBox chkDebug;
+        private System.Windows.Forms.CheckBox chkProtected;
+        private System.Windows.Forms.Label lblVer;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shadow_Text_Editor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            WebClient webClient = new WebClient();
+
+            try
+            {
+                if (!webClient.DownloadString("https://pastebin.com/raw/qwhKxdyX").Contains("1.0"))
+                {
+                    if (MessageBox.Show("There is an update available! Do you want to download it?", "Shadow Text Editor", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) using(var client = new WebClient())
+                    {
+                        Process.Start("UpdaterSTE");
+                        this.Close();
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fileToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if(e.ClickedItem.Name == "openToolStripMenuItem")
+            {
+                BeginInvoke(new Action(() =>
+                {
+                    if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        lblFileName.Text = ofd.FileName;
+                        txbEditor.Text = File.ReadAllText(ofd.FileName);
+
+                        if (chkDebug.Checked)
+                        {
+                            MessageBox.Show(ofd.FileName, "Shadow Text Editor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }));
+            }
+
+            if (e.ClickedItem.Name == "saveAsToolStripMenuItem")
+            {
+                sfd.FileName = ofd.FileName;
+
+                BeginInvoke(new Action(() =>
+                {
+                    if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        StreamWriter write = new StreamWriter(File.Create(sfd.FileName));
+
+                        write.Write(txbEditor.Text);
+                        write.Dispose();
+                    }
+                }));
+            }
+        }
+
+        private void chkProtected_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkProtected.Checked)
+            {
+                txbEditor.Multiline = false;
+                txbEditor.UseSystemPasswordChar = true;
+                this.Size = new System.Drawing.Size(818, 153);
+            }
+            else
+            {
+                txbEditor.Multiline = true;
+                txbEditor.UseSystemPasswordChar = false;
+                this.Size = new System.Drawing.Size(818, 469);
+            }
+        }
+    }
+}
